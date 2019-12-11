3fe2d2ec6f38b7ed710b534df6ecb79574813187
Freddy Estrella Leonor <estrella.freddy@gmail.com>
2019-12-11 18:28:20 -0500

    Initial commit

---
+++ inc/data.php
@@ -0,0 +1,166 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Captain Marvel",
+	"img" => "img/media/captainmarvel.jpg",
+    "genre" => "C",
+    "format" => "Hardcover",
+    "year" => 2000,
+    "category" => "Books",
+    "authors" => [
+        "Marvel Comics"
+    ],
+    "publisher" => "Marvel Comics",
+    "isbn" => '9780374111199'
+];
+$catalog[102] = [
+    "title" => "The Alchemist",
+    "img" => "img/media/pauloCthealchemist.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2006,
+    "category" => "Books",
+    "authors" => [
+        "Paulo Coelho"
+    ],
+    "publisher" => "HarperCollins",
+    "isbn" => '9788576653721'
+];
+$catalog[103] = [
+    "title" => "Veronica decides to die",
+    "img" => "img/media/pauloCvdtdd.jpg",
+    "genre" => "Novel",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Paulo Coelho"
+    ],
+    "publisher" => "HarperCollins",
+    "isbn" => '9781593082499'
+];
+$catalog[104] = [
+    "title" => "Fahrenheit 451",
+    "img" => "img/media/F451.jpg",
+    "genre" => "Science Fiction",
+    "format" => "Paperback",
+    "year" => 1953,
+    "category" => "Books",
+    "authors" => [
+        "Ray Bradbury"
+    ],
+    "publisher" => " Simon & Schuster; Reissue edition (January 10, 2012)",
+    "isbn" => '9781593081157'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Avengers: End Game",
+    "img" => "img/media/avengersendgame.jpeg",
+    "genre" => "Action",
+    "format" => "DVD/Streaming",
+    "year" => 2019,
+    "category" => "Movies",
+    "director" => "Anthony Russo, Joe Russo",
+    "writers" => [
+        "Christopher Markus",
+        "Stephen McFeely"
+    ],
+    "stars" => [
+        "Robert Downey Jr.",
+        "Chris Evans",
+        "Mark Ruffalo",
+    ]
+];
+$catalog[202] = [
+    "title" => "The Matrix",
+    "img" => "img/media/thematrix.jpg",
+    "genre" => "Science Fiction",
+    "format" => "DVD/Streaming",
+    "year" => 1999 ,
+    "category" => "Movies",
+    "director" => " Lana Wachowski, Lilly Wachowski",
+    "writers" => [
+        "Lana Wachowski",
+				"Lilly Wachowski"
+    ],
+    "stars" => [
+			"Keanu Reeves ",
+			"Laurence Fishburne ",
+			"Carrie-Anne Moss "
+    ]
+];
+$catalog[203] = [
+    "title" => "The Matrix: Reloaded",
+    "img" => "img/media/matrixreloaded.jpg",
+    "genre" => "Science Fiction",
+    "format" => "DVD",
+    "year" => 2003,
+    "category" => "Movies",
+    "director" => "Lana Wachowski",
+				"Lilly Wachowski",
+    "writers" => [
+        "Lana Wachowski",
+				"Lilly Wachowski"
+    ],
+    "stars" => [
+		"Keanu Reeves ",
+			"Laurence Fishburne ",
+			  "Carrie-Anne Moss "
+    ]
+];
+$catalog[204] = [
+    "title" => "Pulp Fiction",
+		"img" => "img/media/pulpficton.jpg",
+	  "genre" => "Drama, Science Fiction, Adventure",
+	  "format" => "DVD/Streaming",
+	  "year" => 1994,
+	  "category" => "Movies",
+	  "director" => "Quentin Tarantino",
+	  "writers" => [
+	      "Quentin Tarantino"," Roger Avary"
+
+	  ],
+	  "stars" => [
+	"John Travolta", "Uma Thurman", "Samuel L. Jackson"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "all the small things",
+	"img" => "img/media/blink182_2017.jpg",
+	"genre" =>  "Punk", "Rock",
+	"format" => "CD",
+	"year" => 1999,
+	"category" => "Music",
+	"artist" => "Blink-182"
+];
+
+$catalog[302] = [
+	"title" => "the beautiful people",
+	"img" => "img/media/mamanson.jpg",
+	"genre" => "industrial metal, industrial rock",
+	"format" => "Streaming Services",
+	"year" => 1996,
+	"category" => "Music",
+	"artist" => "Marilyn Manson"
+];
+$catalog[303] = [
+	"title" => "last resort",
+	"img" => "img/media/paparoach.jpg",
+	"genre" => "Alternative metal, Rock",
+	"format" => "CD, Streaming Services",
+	"year" => 2000,
+	"category" => "Music",
+	"artist" => "Papa Roach"
+];
+$catalog[304] = [
+	"title" => "truly madly deeply",
+	"img" => "img/media/savegegarden.jpg",
+	"genre" => "Adult contemporary music, Pop",
+	"format" => "CD, Streaming Services",
+	"year" => 1991,
+	"category" => "Music",
+	"artist" => "Savage Garden"
+];
+?>
